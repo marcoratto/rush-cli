26c17b67fe6626f5a334ea386eec263af81ea9df
IH00294 <marco.ratto@italgas.it>
2024-02-07 13:01:09 +0100

    Fixed for Apple M1

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -1,76 +1,96 @@
 #!/bin/sh
+set -x
 
 # Exit immediately if any command exits with non-zero exit status.
 set -e
 
 if ! command -v unzip >/dev/null; then
-	echo 'err: `unzip` is required to install Rush. Please install it and try again.'
+	echo "err: unzip is required to install Rush. Please install it and try again"
 	exit 1
 fi
 
-if [[ -v RUSH_HOME]]; then
-  rushHome="$RUSH_HOME"
-else
-  if ! -v rush >/dev/null; then
-    rushHome="$HOME/.rush"
-  else
-    rushHome="$(dirname $(dirname $(which rush)))"
+# Check if Rush is already installed; if it is, use the old path for storing,
+# Rush executable
+if ! command -v rush >/dev/null; then
+  binDir="$HOME/.rush/bin"
+  if [ ! -d "$binDir" ]; then
+    mkdir -p "$binDir"
   fi
+else
+  binDir="$(dirname $(which rush))"
 fi
 
+# Set the target and data dir
 if [ "$OS" = "Windows_NT" ]; then
-  target="x86_64-windows"
+  target="win"
+  dataDir="$APPDATA/rush"
 else
   case $(uname -sm) in
-  "Darwin x86_64") target="x86_64-apple-darwin" ;;
-  "Darwin arm64") target="arm64-apple-darwin" ;;
-  *) target="x86_64-linux" ;;
+  "Darwin x86_64")
+    target="mac"
+    dataDir="$HOME/Library/Application Support/rush"
+    ;;
+  "Darwin arm64")
+    target="mac"
+    dataDir="$HOME/Library/Application Support/rush"
+    ;;
+  *)
+    target="linux"
+    dataDir="$HOME/rush"
+    ;;
   esac
 fi
 
 zipUrl="https://github.com/shreyashsaitwal/rush-cli/releases/latest/download/rush-$target.zip"
-curl --location --progress-bar -o "$rushHome/rush-$target.zip" "$zipUrl"
 
-unzip -oq "$rushHome/rush-$target.zip" -d "$rushHome"/
-rm "$rushHome/rush-$target.zip"
+# Download and unzip rush-$target.zip
+curl --location --progress-bar -o "$binDir/rush-$target.zip" "$zipUrl"
+unzip -oq "$binDir/rush-$target.zip" -d "$binDir"/
+rm "$binDir/rush-$target.zip"
 
-# Make the Rush binary executable on Unix systems. 
-if [ ! "$OS" = "Windows_NT" ]; then
-  chmod +x "$rushHome/bin/rush"
+# Delete dataDir if it already exists
+if [ -d "$dataDir" ]; then
+  rm -rf "$dataDir"
 fi
 
-echo
-echo "Successfully downloaded the Rush CLI binary at $rushHome/bin/rush"
+# Then (re-)create it
+mkdir "$dataDir"
 
-# Prompt user of they want to download dev dependencies now.
-echo "Now, proceeding to download necessary Java libraries (approx size: 170 MB)."
-read -p "Do you want to continue? (Y/n) " -n 1 -r
-echo
+# Move the EXEs under the binDir
+mv "$binDir/exe/$target"/* "$binDir"
+rm -r "$binDir/exe/"
 
-if [[ ! $REPLY =~ ^[Nn]$ ]]; then
+# Move all the directories that were unzipped
+mv $(ls -d "$binDir"/*/) "$dataDir"
+
+# Give all the necessary scripts execution permission
+chmod +x "$binDir/rush"
+chmod +x "$dataDir/tools/kotlinc/bin/kotlinc"
+chmod +x "$dataDir/tools/kotlinc/bin/kapt"
+chmod +x "$dataDir/tools/jetifier-standalone/bin/jetifier-standalone"
+
+echo
+echo "Success! Installed Rush at $binDir/rush"
+if ! command -v rush >/dev/null; then
   if [ "$OS" = "Windows_NT" ]; then
-    "./$rushHome/bin/rush.exe" deps sync --dev-deps --no-logo
+    exp=" $dataDir/bin "
+    echo
+    echo "Now, add the following entry to your 'PATH' environment variable:"
   else
-    "./$rushHome/bin/rush" deps sync --dev-deps --no-logo
+    case $SHELL in
+      /bin/zsh) shell_profile=".zshrc" ;;
+      *) shell_profile=".bash_profile" ;;
+    esac
+
+    exp=" export PATH=\"\$PATH:$binDir\" "
+    echo
+    echo "Now, manually add Rush's bin directory to your \$HOME/$shell_profile (or similar):"
   fi
+
+    edge=$(echo " $exp " | sed 's/./-/g')
+    echo "$edge"
+    echo "|$exp|"
+    echo "$edge"
 fi
-
 echo
-if [[ ! $REPLY =~ ^[Nn]$ ]]; then
-  echo "Success! Installed Rush at $rushHome/bin/rush"
-else
-  echo "Rush has been partially installed at $rushHome/bin/rush"
-  echo 'Please run `rush deps sync --dev-deps` to download the necessary Java libraries.'
-fi
-
-case $SHELL in
-  /bin/zsh) shell_profile=".zshrc" ;;
-  *) shell_profile=".bash_profile" ;;
-esac
-
-echo
-echo "Now, add the following to your \$HOME/$shell_profile (or similar):"
-echo "export PATH=\"\$PATH:$rushHome/bin\""
-
-echo
-echo 'Run `rush --help` to get started.'
+echo "Run rush --help to get started."
